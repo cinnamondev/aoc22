272c48bec7be3c5ef6c40a3d98dc2a55891d451a
cinnamondev <justpromptiguess@gmail.com>
2022-12-04 14:38:48 +0000

    Day 4 Improvements
    - Comparing performance against a version using logic operations (kinda clunky)
    - Remade the bit range function, previous function was O(n) (n=difference between low and high range) per value which adds up fast
    - New one uses bit shift right to only get N bits (width= stop-start+1 so 128-stop+start-1), then shifts it by 128-stop to ensure the range ends at the stop position (if index from left, see notes), this is WAY faster

--- src/day4.rs
+++ src/day4.rs
@@ -26,7 +26,7 @@ pub fn parser(input: &str) -> Vec<((u8,u8),(u8,u8))> {
         .collect()
 }
 
-pub fn bit_representation(range: (u8, u8)) -> u128 { // big binary!!
+pub fn dep_bit_representation(range: (u8, u8)) -> u128 { // big binary!!
     let mut to_set = 0b000000000;
     for i in range.0..=range.1 {
         to_set += MSB >> i-1;   // set each bit representing a
@@ -34,13 +34,17 @@ pub fn bit_representation(range: (u8, u8)) -> u128 { // big binary!!
     }
     to_set
 }
+pub fn bit_range(range:(u8,u8)) -> u128 {
+    let (start,stop) = range;
+    (u128::MAX >> (128-stop+start-1)) << (128-stop) // Binary magic
+}
 
-#[aoc(day4, part1)]
+#[aoc(day4, part1, Binary)]
 pub fn solve_part1(input: &[((u8,u8),(u8,u8))]) -> u32 {
     let mut counter = 0;
     for group in input { // groups of two elves
-        let elf_a = bit_representation(group.0);
-        let elf_b = bit_representation(group.1);
+        let elf_a = bit_range(group.0);
+        let elf_b = bit_range(group.1);
         let overlap = elf_a & elf_b;
         if overlap == elf_a || overlap == elf_b {
             counter+=1;
@@ -48,16 +52,42 @@ pub fn solve_part1(input: &[((u8,u8),(u8,u8))]) -> u32 {
     }
     counter
 }
-#[aoc(day4, part2)]
+
+#[aoc(day4, part2, Binary)]
 pub fn solve_part2(input: &[((u8,u8),(u8,u8))]) -> u32 {
     let mut counter = 0;
     for group in input { // groups of two elves
-        let elf_a = bit_representation(group.0);
-        let elf_b = bit_representation(group.1);
+        let elf_a = bit_range(group.0);
+        let elf_b = bit_range(group.1);
         let overlap = elf_a & elf_b;
         if overlap != 0 {
             counter+=1;
         }
     }
     counter
+}
+// Maybe faster? lets find out :)
+#[aoc(day4, part1, Logic)]
+pub fn logic_solve_part1(input: &[((u8,u8),(u8,u8))]) -> u32 {
+    input.iter()
+        .fold(0, |acc, &group| {
+            let (a,b) = group;
+            if (a.0 >= b.0 && a.1 <= b.1) || (b.0 >= a.0 && b.1 <= a.1) {
+                acc+1
+            } else {
+                acc
+            }
+        })
+}
+#[aoc(day4, part2, Logic)]
+pub fn logic_solve_part2(input: &[((u8,u8),(u8,u8))]) -> u32 {
+    input.iter()
+        .fold(0, |acc, &group| {
+            let (a, b) = group;
+            if (a.0 >= b.0 && a.0 <= b.1) || (b.0 >= a.0 && b.0 <= a.1) {
+                acc + 1
+            } else {
+                acc
+            }
+        })
 }
